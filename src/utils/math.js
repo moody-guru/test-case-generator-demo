a93a52690ea56c1e246d5ba7cbbf2270799c0d55
Pushkar Sahu <153001817+moody-guru@users.noreply.github.com>
2025-08-08 11:05:48 +0530

    Update math.js

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,1 +1,7 @@
-export const add = (a, b) => a + b;
+// src/utils/math.js
+export const add = (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Inputs must be numbers');
+  }
+  return a + b;
+};
